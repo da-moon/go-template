903ab4b3281b54eb724888c8eb009853c10b3d18
havi <contact@havi.dev>
2022-02-06 16:44:32 +0000

    feat(tools): Golang tools dependenies

---
+++ go.mod
@@ -0,0 +1,24 @@
+module github.com/da-moon/go-template
+
+go 1.16
+
+require (
+	github.com/davidrjenni/reftools v0.0.0-20210213085015-40322ffdc2e4
+	github.com/fatih/gomodifytags v1.13.0
+	github.com/fatih/motion v1.1.0
+	github.com/go-delve/delve v1.6.1
+	github.com/golangci/golangci-lint v1.41.1
+	github.com/josharian/impl v1.1.0
+	github.com/jstemmer/gotags v1.4.1
+	github.com/kisielk/errcheck v1.6.0
+	github.com/klauspost/asmfmt v1.3.1
+	github.com/koron/iferr v0.0.0-20180615142939-bb332a3b1d91
+	github.com/magefile/mage v1.11.0
+	github.com/rogpeppe/godef v1.1.2
+	github.com/stretchr/commander v0.0.0-20130925004555-18f86d1c1da1 // indirect
+	github.com/stretchr/gorc v0.0.0-20130924145046-6dc9c25595e6
+	golang.org/x/lint v0.0.0-20210508222113-6edffad5e616
+	golang.org/x/tools v0.1.5
+	golang.org/x/tools/gopls v0.7.0
+	honnef.co/go/tools v0.2.0
+)
---
+++ tools.go
@@ -0,0 +1,53 @@
+// +build tools
+
+package tools
+
+// Manage tool dependencies via go.mod.
+//
+// https://github.com/golang/go/wiki/Modules#how-can-i-track-tool-dependencies-for-a-module
+// https://github.com/golang/go/issues/25922
+//
+// nolint
+import (
+	_ "github.com/davidrjenni/reftools/cmd/fillstruct"
+	_ "github.com/fatih/gomodifytags"
+	_ "github.com/fatih/motion"
+	_ "github.com/go-delve/delve/cmd/dlv"
+	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
+	_ "github.com/josharian/impl"
+	_ "github.com/jstemmer/gotags"
+	_ "github.com/kisielk/errcheck"
+	_ "github.com/klauspost/asmfmt/cmd/asmfmt"
+	_ "github.com/koron/iferr"
+	_ "github.com/magefile/mage"
+	_ "github.com/rogpeppe/godef"
+	_ "github.com/stretchr/gorc"
+	_ "golang.org/x/lint/golint"
+	_ "golang.org/x/tools/cmd/goimports"
+	_ "golang.org/x/tools/cmd/gorename"
+	_ "golang.org/x/tools/cmd/guru"
+	_ "golang.org/x/tools/gopls"
+	_ "honnef.co/go/tools/cmd/keyify"
+	_ "honnef.co/go/tools/cmd/staticcheck"
+)
+
+//go:generate go install -v "github.com/davidrjenni/reftools/cmd/fillstruct"
+//go:generate go install -v "github.com/fatih/gomodifytags"
+//go:generate go install -v "github.com/fatih/motion"
+//go:generate go install -v "github.com/go-delve/delve/cmd/dlv"
+//go:generate go install -v "github.com/josharian/impl"
+//go:generate go install -v "github.com/jstemmer/gotags"
+//go:generate go install -v "github.com/kisielk/errcheck"
+//go:generate go install -v "github.com/klauspost/asmfmt/cmd/asmfmt"
+//go:generate go install -v "github.com/koron/iferr"
+//go:generate go install -v "github.com/magefile/mage"
+//go:generate go install -v "github.com/rogpeppe/godef"
+//go:generate go install -v "github.com/stretchr/gorc"
+//go:generate go install -v "golang.org/x/lint/golint"
+//go:generate go install -v "golang.org/x/tools/cmd/goimports"
+//go:generate go install -v "golang.org/x/tools/cmd/gorename"
+//go:generate go install -v "golang.org/x/tools/cmd/guru"
+//go:generate go install -v "golang.org/x/tools/gopls"
+//go:generate go install -v "honnef.co/go/tools/cmd/keyify"
+//go:generate go install -v "honnef.co/go/tools/cmd/staticcheck"
+//go:generate go install -v "github.com/golangci/golangci-lint/cmd/golangci-lint@latest"
